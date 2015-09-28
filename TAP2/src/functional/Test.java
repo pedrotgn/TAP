7dd2cb576dd5fbff097634dd99193e245c1d7386
milax <milax@d200.lab.deim>
2015-09-28 11:40:38 +0200

    java 8 samples

--- TAP2/src/functional/Test.java
+++ TAP2/src/functional/Test.java
@@ -10,16 +10,16 @@ public class Test {
     public static void main(String args[]){
         List<Animal> list = new LinkedList<Animal>();
 
-        Animal a1 = new Animal(3);
-        Animal a2 = new Animal(7);
-        Animal a3 = new Animal(1);
+        Animal a1 = new Animal("a",3);
+        Animal a2 = new Animal("b",7);
+        Animal a3 = new Animal("c",1);
 
         list.add(a1);
         list.add(a2);
         list.add(a3);
 
         list.forEach(elem->elem.talk());
-
+        list.forEach(System.out::println);
 
 
 
