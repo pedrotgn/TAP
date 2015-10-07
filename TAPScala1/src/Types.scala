57c24c24d37d9a5c9108d18e959c3abd47368141
Pedro <pedro@kafre.(none)>
2015-10-07 13:01:38 +0200

    Scala first lab

---
+++ TAPScala1/src/Types.scala
@@ -0,0 +1,18 @@
+/**
+ * Created by pedro on 7/28/15.
+ */
+
+
+object Types {
+  def main(args: Array[String]) {
+   var x = 4
+   val s = "pedro"
+   val sum = 1+2
+
+   println(sum)
+
+   // sum = 5
+    x = 6
+
+  }
+}
