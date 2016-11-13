d773629d5886ad0d0bc75e38a9743efcc726fee9
Pedro <pedrotgn@Pedros-MacBook-Air.local>
2016-11-13 11:35:34 +0100

    fixed solutions

--- TAPScalaF/src/solutions/Ex1.scala
+++ TAPScalaF/src/solutions/Ex1.scala
@@ -26,6 +26,11 @@ object Ex1 extends scala.App {
 
   def even(x:Int):Boolean = x%2==0
 
+  def beginsWithA(s:String) = s.startsWith("a")
+
+  def capitals (s:String) = s.toUpperCase
+
+
   println (ex2(range.toList,even,elem=>elem*elem))
 
 
@@ -34,13 +39,26 @@ object Ex1 extends scala.App {
   println (evenapply(range.toList,elem=>elem*elem))
 
 
-  def ex3(f1:Int=>Boolean) (f2:Int=>Int) (l:List[Int])= l.filter(f1).map(f2)
+  def ex3 [T] (f1:T=>Boolean) (f2:T=>T) (l:List[T])= l.filter(f1).map(f2)
 
   println (ex3 (even) (elem=>elem*elem) (range.toList))
 
   val evenapply2 = ex3 (even) (elem=>elem*elem) _
 
   println (evenapply2  (range.toList))
+
+  val lists = List("abc","def","ghi")
+
+  println (ex3 ((elem:String)=>elem.startsWith("a")) ((elem:String)=>elem.toUpperCase()) (lists))
+  val evenapply3 = ex3 ((elem:String)=>elem.startsWith("a")) ((elem:String)=>elem.toUpperCase()) _
+
+  println (evenapply3(lists))
+
+  val evenapply4 = ex3 (beginsWithA) (capitals) _
+
+  println (evenapply4(lists))
+
+
 
 
 
