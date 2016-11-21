347b64b187e9e5a20b6b11c3dcbf12cfff3a6f1c
pedrotgn <astlab144>
2016-11-21 17:44:14 +0100

    zipwith sample in ListRecursion

--- TAPScalaF/src/ListRecursion.scala
+++ TAPScalaF/src/ListRecursion.scala
@@ -1,3 +1,5 @@
+import _root_.scala.collection.immutable.::
+
 /**
  * Created by pedro on 9/4/15.
  */
@@ -16,6 +18,13 @@ object ListRecursion extends scala.App{
     case x :: xs => tlength(acc+1,xs)
   }
 
+  def zipWith[T](list: List[T],list2:List[T],f:(T,T)=>T): List[T] = (list,list2) match {
+    case (Nil,_) => Nil
+    case (a :: b, c::d)  => f(a,c):: zipWith(b,d,f)
+
+  }
+
+
   val long = tlength(0,_:List[Any])
 
 
@@ -24,5 +33,7 @@ object ListRecursion extends scala.App{
     println(long(List(1,2,3,4)))
     println(length(List("a","b","c","d","e")))
     println(length("pedro".toList))
+    println(zipWith(List(1,2,3),List(4,5,6),(x:Int,y:Int)=>x+y))
+
 
 }
