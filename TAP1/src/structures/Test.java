8c6c26eb7d7d96371692e904a07f77592af2cf35
Daniel Barcelona <daniel.barcelona@urv.cat>
2020-11-03 12:20:31 +0100

    clean TAP1

--- TAP1/src/structures/Test.java
+++ TAP1/src/structures/Test.java
@@ -2,40 +2,36 @@ package structures;
 
 import java.util.*;
 
-public class Test
-{
-   
-  
-   
-   public static void main( String args[] ) 
-   {
-   		Collection<String> c;
-    	
-    	c = new ArrayList<String>();
-    	test(c);
-    	c = new LinkedList<String>();
-    	test(c);
-    	c = new HashSet<String>();
-    	test(c);
-     	c = new TreeSet<String>();
-    	test(c);
-    	
-   }
+public class Test {
 
- public static void test(Collection<String> c)
-   {
-   	 
-     String strings[] = {"uno", "dos" , "tres", "cuatro", "tres"};
-     
-     
-     for(String num:strings)
-         c.add(num);                       
-     
-     for(String num:c)
-           System.out.println(num);
-     
-     System.out.println( "-------------------------");
 
-   	
-   }
+    public static void main(String args[]) {
+        Collection<String> c;
+
+        c = new ArrayList<String>();
+        test(c);
+        c = new LinkedList<String>();
+        test(c);
+        c = new HashSet<String>();
+        test(c);
+        c = new TreeSet<String>();
+        test(c);
+
+    }
+
+    public static void test(Collection<String> c) {
+
+        String strings[] = {"uno", "dos", "tres", "cuatro", "tres"};
+
+
+        for (String num : strings)
+            c.add(num);
+
+        for (String num : c)
+            System.out.println(num);
+
+        System.out.println("-------------------------");
+
+
+    }
 } 
