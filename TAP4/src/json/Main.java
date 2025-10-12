b788a050922e713824d834de88949e82524457f1
Enrique Molina <enrique.molina@urv.cat>
2025-10-12 20:54:21 +0200

    Add GSON parsing example

---
+++ TAP4/src/json/Main.java
@@ -0,0 +1,21 @@
+package json;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        try (FileReader reader = new FileReader("TAP4/measurements.json")) {
+            Gson gson = new Gson();
+            Type listType = new TypeToken<List<Measurement>>() {}.getType();
+            List<Measurement> measurements = gson.fromJson(reader, listType);
+
+            measurements.forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
