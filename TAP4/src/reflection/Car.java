a3ef02c24787e0a73708e6bdbe91d900422e6749
Enrique Molina <enrique.molina@urv.cat>
2025-10-12 22:55:57 +0200

    Add reflect.Field example

---
+++ TAP4/src/reflection/Car.java
@@ -0,0 +1,15 @@
+package reflection;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int year;
+    private double mileage;
+
+    public Car(String brand, String model, int year, double mileage) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+    }
+}
